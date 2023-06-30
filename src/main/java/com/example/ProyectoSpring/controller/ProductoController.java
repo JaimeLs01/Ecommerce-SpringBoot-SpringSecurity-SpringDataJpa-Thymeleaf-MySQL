97fcdc108842cd90171dd641289756955c6ecab3
Jaime Leon <n00175966@upn.pe>
2023-06-30 01:23:49 +0100

    crear repository,ProductoController y la vista show.html

---
+++ src/main/java/com/example/ProyectoSpring/controller/ProductoController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.ProyectoSpring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/productos")
+public class ProductoController {
+    @GetMapping("")
+    public String show(){
+        return "/productos/show";
+    }
+}
