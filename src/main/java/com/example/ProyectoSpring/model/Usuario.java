bbe81bdf28ede5eefd65ef7d8e1d1f0194b53b04
Jaime Leon <n00175966@upn.pe>
2023-07-01 06:26:25 +0100

    creacion de guardar producto

--- src/main/java/com/example/ProyectoSpring/model/Usuario.java
+++ src/main/java/com/example/ProyectoSpring/model/Usuario.java
@@ -28,7 +28,7 @@ public class Usuario {
     @OneToMany(mappedBy = "usuario")
     private List<Orden> ordenes;
 
-    public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono, String tipo, String password, List<Producto> productos, List<Orden> ordenes) {
+    public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono, String tipo, String password) {
         this.id = id;
         this.nombre = nombre;
         this.username = username;
@@ -37,8 +37,6 @@ public class Usuario {
         this.telefono = telefono;
         this.tipo = tipo;
         this.password = password;
-        this.productos = productos;
-        this.ordenes = ordenes;
     }
 
     public Usuario() {
