748b99885d6f650a5386d73e5c044fa29cce9ca7
Jaime Leon <n00175966@upn.pe>
2023-07-06 07:33:00 +0100

    Agregamos la funcionalidad para poder observar la informacion de un producto desde la vista del usuario

--- src/main/java/com/example/ProyectoSpring/controller/HomeController.java
+++ src/main/java/com/example/ProyectoSpring/controller/HomeController.java
@@ -7,22 +7,35 @@ package com.example.ProyectoSpring.controller;
 import com.example.ProyectoSpring.model.Producto;
 import com.example.ProyectoSpring.service.ProductoService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+/**
+ *
+ * @author User
+ */
 @Controller
-@RequestMapping("/administrador")
-public class AdministradorController {
+@RequestMapping("/")
+public class HomeController {
     @Autowired
-    private ProductoService productoService;
+    private ProductoService productoService; 
     @GetMapping("")
     public String home(Model model){
-        List<Producto> productos=productoService.findAll();
-        model.addAttribute("productos", productos);
-        return "administrador/home";
+        List<Producto> productos = productoService.findAll();
+        model.addAttribute("productos",productos);
+        return "usuario/home";
+    }
+    @GetMapping("/productohome/{id}")
+    public String productoHome(@PathVariable Integer id,Model model){
+        Producto producto;
+        Optional<Producto> optionalProducto= productoService.get(id);
+        producto=optionalProducto.get();
+        model.addAttribute("producto", producto);
+        return "usuario/productohome";
     }
 }
