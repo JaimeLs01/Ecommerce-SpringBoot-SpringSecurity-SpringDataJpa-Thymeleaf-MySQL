7829cd2aa4e110a53ae8a3ff594e3611f322dc89
Jaime Leon <n00175966@upn.pe>
2023-07-06 19:34:05 +0100

    Agregamos la validacion para que al momento de agregar un producto al carrito no se pueda repetir 2 veces

--- src/main/java/com/example/ProyectoSpring/controller/HomeController.java
+++ src/main/java/com/example/ProyectoSpring/controller/HomeController.java
@@ -27,60 +27,78 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
     @Autowired
-    private ProductoService productoService; 
-    
+    private ProductoService productoService;
+
     List<DetalleOrden> detalles = new ArrayList<>();
-    Orden orden=new Orden();
+    Orden orden = new Orden();
+
     @GetMapping("")
-    public String home(Model model){
+    public String home(Model model) {
         List<Producto> productos = productoService.findAll();
-        model.addAttribute("productos",productos);
+        model.addAttribute("productos", productos);
         return "usuario/home";
     }
+
     @GetMapping("/productohome/{id}")
-    public String productoHome(@PathVariable Integer id,Model model){
+    public String productoHome(@PathVariable Integer id, Model model) {
         Producto producto;
-        Optional<Producto> optionalProducto= productoService.get(id);
-        producto=optionalProducto.get();
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
         model.addAttribute("producto", producto);
         return "usuario/productohome";
     }
+
     @PostMapping("/addCart")
-    public String addCart(@RequestParam("id") Integer id,@RequestParam("cantidad") Integer cantidad){
+    public String addCart(@RequestParam("id") Integer id, @RequestParam("cantidad") Integer cantidad) {
         Producto producto;
-        double sumaTotal=0;
         Optional<Producto> optionalProducto = productoService.get(id);
         producto = optionalProducto.get();
-        DetalleOrden detalleOrden= new DetalleOrden();
-        detalleOrden.setCantidad(cantidad);
-        detalleOrden.setNombre(producto.getNombre());
-        detalleOrden.setPrecio(producto.getPrecio());
-        detalleOrden.setTotal(cantidad*producto.getPrecio());
-        detalleOrden.setProducto(producto);
-        detalles.add(detalleOrden);
-        orden.setTotal(ObtenerTotal());
+        DetalleOrden detalleOrden = new DetalleOrden();
+        if (ValidarProductoExiste(producto.getId()) == null) {
+            detalleOrden.setCantidad(cantidad);
+            detalleOrden.setNombre(producto.getNombre());
+            detalleOrden.setPrecio(producto.getPrecio());
+            detalleOrden.setTotal(cantidad * producto.getPrecio());
+            detalleOrden.setProducto(producto);
+            detalles.add(detalleOrden);
+            orden.setTotal(ObtenerTotal());
+        }
         return "redirect:/cart";
     }
+
     @GetMapping("/deleteCart/{id}")
-    public String deleteCart(@PathVariable Integer id){
-        for(int i=0;i<detalles.size();i++){
-            if(detalles.get(i).getProducto().getId().equals(id)){
+    public String deleteCart(@PathVariable Integer id) {
+        for (int i = 0; i < detalles.size(); i++) {
+            if (detalles.get(i).getProducto().getId().equals(id)) {
                 detalles.remove(i);
             }
         }
         orden.setTotal(ObtenerTotal());
         return "redirect:/cart";
     }
-    double ObtenerTotal(){
+
+    double ObtenerTotal() {
         double sumaTotal = 0;
-        for(int i =0;i<detalles.size();i++){
-            sumaTotal+=detalles.get(i).getTotal();
+        for (int i = 0; i < detalles.size(); i++) {
+            sumaTotal += detalles.get(i).getTotal();
         }
         return sumaTotal;
     }
+
+    Integer ValidarProductoExiste(Integer id) {
+        Integer posicion = null;
+        for (int i = 0; i < detalles.size(); i++) {
+            if (detalles.get(i).getProducto().getId().equals(id)) {
+                posicion = i;
+            }
+        }
+        return posicion;
+    }
+
     @GetMapping("/cart")
-    public String cart(Model model){
+    public String cart(Model model) {
         model.addAttribute("detalles", detalles);
         model.addAttribute("total", orden.getTotal());
         return "usuario/carrito";
